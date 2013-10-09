87167ea03bf54c25e83e10a20f1ca5d9c7643a50
raphaelstary <raphael.stary@gmail.com>
2013-10-09 22:42:48 +0200

    layer factory creates knockout view model

--- test/spec/FactorySpec.js
+++ test/spec/FactorySpec.js
@@ -1,4 +1,4 @@
-define(['view/Factory', 'lib/knockout'], function (Factory, ko) {
+define(['view/Factory', 'lib/knockout', 'view/Layer'], function (Factory, ko, Layer) {
 
     describe('view model creation', function () {
 
@@ -58,7 +58,36 @@ define(['view/Factory', 'lib/knockout'], function (Factory, ko) {
 
             expect(actual().length).toBe(1);
             expect(actual()[0]).toBeDefined();
-            expect(actual()[0].name).toBe('one');
+            expect(actual()[0] instanceof Layer).toBeTruthy();
+            expect(actual()[0].name()).toBe('one');
         });
+
+        it('should create an observable array containing two layers, ' +
+            'when I call create, ' +
+            'given an array with two layers', function () {
+
+            var input = [
+                {
+                    name: 'one'
+                },
+                {
+                    name: 'two'
+                }
+            ];
+
+            var factory = new Factory(input);
+
+            var actual = factory.createLayerModel();
+
+            expect(actual().length).toBe(2);
+
+            expect(actual()[0] instanceof Layer).toBeTruthy();
+            expect(actual()[0].name()).toBe('one');
+
+            expect(actual()[1] instanceof Layer).toBeTruthy();
+            expect(actual()[1].name()).toBe('two');
+        });
+
+
     });
 });
--- www/js/view/Factory.js
+++ www/js/view/Factory.js
@@ -1,10 +1,20 @@
-define(['lib/knockout'], function (ko) {
-    function Factory() {
-
+define(['lib/knockout', 'view/Layer'], function (ko, Layer) {
+    function Factory(list) {
+        this.list = list;
     }
 
     Factory.prototype.createLayerModel = function () {
-        return ko.observableArray();
+        var result = ko.observableArray();
+
+        if (this.list === undefined || this.list === null || this.list.length === 0) {
+            return result;
+        }
+
+        this.list.forEach(function (layer) {
+            result.push(new Layer(layer.id, layer.name));
+        });
+
+        return result;
     };
 
     return Factory;
