08cb939e1f49ca24062789f31a70130b223b3f85
raphaelstary <raphael.stary@gmail.com>
2013-10-24 15:52:53 +0200

    action interpreter interprets 9 action points

--- test/spec/input/ActionInterpreterSpec.js
+++ test/spec/input/ActionInterpreterSpec.js
@@ -38,22 +38,6 @@ define(['input/ActionInterpreter', 'input/PointerAction', 'view/Item'], function
 
     describe("as a user I click on the 'top' action point to start the edit action", function () {
 
-        it("should return 'NOTHING' " +
-            "when I miss the action point",  function () {
-
-            var checkCollision = function () {
-                return false;
-            };
-
-            var cut = new ActionInterpreter(checkCollision);
-
-            var item = new Item('itemOne', 100, 100, 100, 100);
-
-            var actual = cut.interpret({}, item);
-
-            expect(actual).toBe(PointerAction.NOTHING);
-        });
-
         it("should return 'RESIZE_TOP' " +
             "when I hit the action point",  function () {
 
@@ -72,22 +56,6 @@ define(['input/ActionInterpreter', 'input/PointerAction', 'view/Item'], function
     });
 
     describe("as a user I click on the 'top right' action point to start the edit action", function () {
-
-        it("should return 'NOTHING' " +
-            "when I miss the action point",  function () {
-
-            var checkCollision = function () {
-                return false;
-            };
-
-            var cut = new ActionInterpreter(checkCollision);
-
-            var item = new Item('itemOne', 100, 100, 100, 100);
-
-            var actual = cut.interpret({}, item);
-
-            expect(actual).toBe(PointerAction.NOTHING);
-        });
 
         it("should return 'RESIZE_TOP_RIGHT' " +
             "when I hit the action point",  function () {
@@ -108,22 +76,6 @@ define(['input/ActionInterpreter', 'input/PointerAction', 'view/Item'], function
 
     describe("as a user I click on the 'left' action point to start the edit action", function () {
 
-        it("should return 'NOTHING' " +
-            "when I miss the action point",  function () {
-
-            var checkCollision = function () {
-                return false;
-            };
-
-            var cut = new ActionInterpreter(checkCollision);
-
-            var item = new Item('itemOne', 100, 100, 100, 100);
-
-            var actual = cut.interpret({}, item);
-
-            expect(actual).toBe(PointerAction.NOTHING);
-        });
-
         it("should return 'RESIZE_LEFT' " +
             "when I hit the action point",  function () {
 
@@ -138,6 +90,101 @@ define(['input/ActionInterpreter', 'input/PointerAction', 'view/Item'], function
             var actual = cut.interpret({}, item);
 
             expect(actual).toBe(PointerAction.RESIZE_LEFT);
+        });
+    });
+
+    describe("as a user I click on the 'move' action point to start the edit action", function () {
+
+        it("should return 'MOVE' " +
+            "when I hit the action point",  function () {
+
+            var checkCollision = function (pointer, actionPointAsRect) {
+                return isCenter(actionPointAsRect, item);
+            };
+
+            var cut = new ActionInterpreter(checkCollision);
+
+            var item = new Item('itemOne', 100, 100, 100, 100);
+
+            var actual = cut.interpret({}, item);
+
+            expect(actual).toBe(PointerAction.MOVE);
+        });
+    });
+
+    describe("as a user I click on the 'right' action point to start the edit action", function () {
+
+        it("should return 'RESIZE_RIGHT' " +
+            "when I hit the action point",  function () {
+
+            var checkCollision = function (pointer, actionPointAsRect) {
+                return isRight(actionPointAsRect, item);
+            };
+
+            var cut = new ActionInterpreter(checkCollision);
+
+            var item = new Item('itemOne', 100, 100, 100, 100);
+
+            var actual = cut.interpret({}, item);
+
+            expect(actual).toBe(PointerAction.RESIZE_RIGHT);
+        });
+    });
+
+    describe("as a user I click on the 'bottom left' action point to start the edit action", function () {
+
+        it("should return 'RESIZE_BOTTOM_LEFT' " +
+            "when I hit the action point",  function () {
+
+            var checkCollision = function (pointer, actionPointAsRect) {
+                return isBottomLeft(actionPointAsRect, item);
+            };
+
+            var cut = new ActionInterpreter(checkCollision);
+
+            var item = new Item('itemOne', 100, 100, 100, 100);
+
+            var actual = cut.interpret({}, item);
+
+            expect(actual).toBe(PointerAction.RESIZE_BOTTOM_AND_LEFT);
+        });
+    });
+
+    describe("as a user I click on the 'bottom' action point to start the edit action", function () {
+
+        it("should return 'RESIZE_BOTTOM' " +
+            "when I hit the action point",  function () {
+
+            var checkCollision = function (pointer, actionPointAsRect) {
+                return isBottom(actionPointAsRect, item);
+            };
+
+            var cut = new ActionInterpreter(checkCollision);
+
+            var item = new Item('itemOne', 100, 100, 100, 100);
+
+            var actual = cut.interpret({}, item);
+
+            expect(actual).toBe(PointerAction.RESIZE_BOTTOM);
+        });
+    });
+
+    describe("as a user I click on the 'bottom right' action point to start the edit action", function () {
+
+        it("should return 'RESIZE_BOTTOM_RIGHT' " +
+            "when I hit the action point",  function () {
+
+            var checkCollision = function (pointer, actionPointAsRect) {
+                return isBottomRight(actionPointAsRect, item);
+            };
+
+            var cut = new ActionInterpreter(checkCollision);
+
+            var item = new Item('itemOne', 100, 100, 100, 100);
+
+            var actual = cut.interpret({}, item);
+
+            expect(actual).toBe(PointerAction.RESIZE_BOTTOM_AND_RIGHT);
         });
     });
 
@@ -167,5 +214,40 @@ define(['input/ActionInterpreter', 'input/PointerAction', 'view/Item'], function
             actionPoint.pointA.yPoint === rectItem.yPoint() + (rectItem.height()/2) - 2 &&
             actionPoint.pointB.xPoint === rectItem.xPoint() + 2 &&
             actionPoint.pointB.yPoint === rectItem.yPoint() + (rectItem.height()/2) + 2;
+    }
+
+    function isCenter(actionPoint, rectItem) {
+        return actionPoint.pointA.xPoint === rectItem.xPoint() + (rectItem.width()/2) - 2 &&
+            actionPoint.pointA.yPoint === rectItem.yPoint() + (rectItem.height()/2) - 2 &&
+            actionPoint.pointB.xPoint === rectItem.xPoint() + (rectItem.width()/2) + 2 &&
+            actionPoint.pointB.yPoint === rectItem.yPoint() + (rectItem.height()/2) + 2;
+    }
+
+    function isRight(actionPoint, rectItem) {
+        return actionPoint.pointA.xPoint === rectItem.xPoint() + rectItem.width() - 2 &&
+            actionPoint.pointA.yPoint === rectItem.yPoint() + (rectItem.height()/2) - 2 &&
+            actionPoint.pointB.xPoint === rectItem.xPoint() + rectItem.width() + 2 &&
+            actionPoint.pointB.yPoint === rectItem.yPoint() + (rectItem.height()/2) + 2;
+    }
+
+    function isBottomLeft(actionPoint, rectItem) {
+        return actionPoint.pointA.xPoint === rectItem.xPoint()  - 2 &&
+            actionPoint.pointA.yPoint === rectItem.yPoint() + rectItem.height() - 2 &&
+            actionPoint.pointB.xPoint === rectItem.xPoint() + 2 &&
+            actionPoint.pointB.yPoint === rectItem.yPoint() + rectItem.height() + 2;
+    }
+
+    function isBottom(actionPoint, rectItem) {
+        return actionPoint.pointA.xPoint === rectItem.xPoint() + (rectItem.width()/2) - 2 &&
+            actionPoint.pointA.yPoint === rectItem.yPoint() + rectItem.height() - 2 &&
+            actionPoint.pointB.xPoint === rectItem.xPoint() + (rectItem.width()/2) + 2 &&
+            actionPoint.pointB.yPoint === rectItem.yPoint() + rectItem.height() + 2;
+    }
+
+    function isBottomRight(actionPoint, rectItem) {
+        return actionPoint.pointA.xPoint === rectItem.xPoint() + rectItem.width() - 2 &&
+            actionPoint.pointA.yPoint === rectItem.yPoint() + rectItem.height() - 2 &&
+            actionPoint.pointB.xPoint === rectItem.xPoint() + rectItem.width() + 2 &&
+            actionPoint.pointB.yPoint === rectItem.yPoint() + rectItem.height() + 2;
     }
 });
--- www/js/input/ActionInterpreter.js
+++ www/js/input/ActionInterpreter.js
@@ -29,6 +29,20 @@ define(['input/PointerAction', 'input/ABRectangle', 'input/Point'], function (Po
         } else if (this._checkCollision(pointer, this._getLeftPoint(item))) {
             return PointerAction.RESIZE_LEFT;
 
+        } else if (this._checkCollision(pointer, this._getCenterPoint(item))) {
+            return PointerAction.MOVE;
+
+        } else if (this._checkCollision(pointer, this._getRightPoint(item))) {
+            return PointerAction.RESIZE_RIGHT;
+
+        } else if (this._checkCollision(pointer, this._getBottomLeftPoint(item))) {
+            return PointerAction.RESIZE_BOTTOM_AND_LEFT;
+
+        } else if (this._checkCollision(pointer, this._getBottomPoint(item))) {
+            return PointerAction.RESIZE_BOTTOM;
+
+        } else if (this._checkCollision(pointer, this._getBottomRightPoint(item))) {
+            return PointerAction.RESIZE_BOTTOM_AND_RIGHT;
         }
 
         return PointerAction.NOTHING;
@@ -52,6 +66,31 @@ define(['input/PointerAction', 'input/ABRectangle', 'input/Point'], function (Po
     ActionInterpreter.prototype._getLeftPoint = function (item) {
         return new ABRectangle(new Point(item.xPoint() - 2, item.yPoint() + (item.height() / 2) - 2),
             new Point(item.xPoint() + 2, item.yPoint() + (item.height() / 2) + 2));
+    };
+
+    ActionInterpreter.prototype._getCenterPoint = function (item) {
+        return new ABRectangle(new Point(item.xPoint() + (item.width() / 2) - 2, item.yPoint() + (item.height() / 2) - 2),
+            new Point(item.xPoint() + (item.width() / 2) + 2, item.yPoint() + (item.height() / 2) + 2));
+    };
+
+    ActionInterpreter.prototype._getRightPoint = function (item) {
+        return new ABRectangle(new Point(item.xPoint() + item.width() - 2, item.yPoint() + (item.height() / 2) - 2),
+            new Point(item.xPoint() + item.width() + 2, item.yPoint() + (item.height() / 2) + 2));
+    };
+
+    ActionInterpreter.prototype._getBottomLeftPoint = function (item) {
+        return new ABRectangle(new Point(item.xPoint() - 2, item.yPoint() + item.height() - 2),
+            new Point(item.xPoint() + 2, item.yPoint() + item.height() + 2));
+    };
+
+    ActionInterpreter.prototype._getBottomPoint = function (item) {
+        return new ABRectangle(new Point(item.xPoint() + (item.width() / 2) - 2, item.yPoint() + item.height() - 2),
+            new Point(item.xPoint() + (item.width() / 2) + 2, item.yPoint() + item.height() + 2));
+    };
+
+    ActionInterpreter.prototype._getBottomRightPoint = function (item) {
+        return new ABRectangle(new Point(item.xPoint() + item.width() - 2, item.yPoint() + item.height() - 2),
+            new Point(item.xPoint() + item.width() + 2, item.yPoint() + item.height() + 2));
     };
 
     return ActionInterpreter;
