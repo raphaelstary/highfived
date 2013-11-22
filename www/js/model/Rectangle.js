4d4fdd0335d1505c4fecb372abf08caabadedb0b
raphaelstary <raphael.stary@gmail.com>
2013-11-22 13:37:03 +0100

    fixes string parse number issue with input fields

--- www/js/model/Rectangle.js
+++ www/js/model/Rectangle.js
@@ -11,10 +11,10 @@ define(['lib/knockout', 'model/Item'], function (ko, Item) {
     function Rectangle(name, xPoint, yPoint, width, height) {
         Item.call(this, name);
 
-        this.xPoint = ko.observable(xPoint);
-        this.yPoint = ko.observable(yPoint);
-        this.width = ko.observable(width);
-        this.height = ko.observable(height);
+        this.xPoint = ko.observable(xPoint).extend({integer: null});
+        this.yPoint = ko.observable(yPoint).extend({integer: null});
+        this.width = ko.observable(width).extend({integer: null});
+        this.height = ko.observable(height).extend({integer: null});
     }
 
     Rectangle.prototype = Object.create(Item.prototype);
--- www/js/view/MainView.js
+++ www/js/view/MainView.js
@@ -1,6 +1,34 @@
 define(['lib/knockout', 'view/ControlBarView', 'view/LayerToolView', 'model/Factory', 'view/LayerItemFilter'],
     function (ko, ControlBarView, LayerToolView, Factory, LayerItemFilter) {
 
+        // filter for numeric input fields
+        ko.extenders.integer = function(target) {
+            //create a writeable computed observable to intercept writes to our observable
+            var result = ko.computed({
+                read: target,  //always return the original observables value
+                write: function(newValue) {
+                    var current = target(),
+                        newValueAsNum = isNaN(newValue) ? 0 : parseInt(newValue);
+
+                    //only write if it changed
+                    if (newValueAsNum !== current) {
+                        target(newValueAsNum);
+                    } else {
+                        //if the rounded value is the same, but a different value was written, force a notification for the current field
+                        if (newValue !== current) {
+                            target.notifySubscribers(newValueAsNum);
+                        }
+                    }
+                }
+            }).extend({ notify: 'always' });
+
+            //initialize with current value to make sure it is rounded appropriately
+            result(target());
+
+            //return the new computed observable
+            return result;
+        };
+
         function MainView(inputLayers) {
             this.inputLayers = inputLayers;
         }
