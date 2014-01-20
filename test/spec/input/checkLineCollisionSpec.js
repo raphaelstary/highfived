2fa5618611892c035f858dadae90706f5df139d6
raphaelstary <raphael.stary@gmail.com>
2014-01-20 17:12:30 +0100

    check line collision test case first hit

--- test/spec/input/checkLineCollisionSpec.js
+++ test/spec/input/checkLineCollisionSpec.js
@@ -15,6 +15,12 @@ define(['input/checkLineCollision'], function (checkLineCollision) {
         it("should return true " +
             "when a pointer hits a line on the left side of the line", function () {
 
+            var line = {xPointA: 10, yPointA: 10, xPointB: 50, yPointB: 10};
+            var pointer = {xPoint: 30, yPoint: 14, radius: 4};
+
+            var actual = checkLineCollision(line, pointer);
+
+            expect(actual).toBe(true);
         });
 
         it("should return true " +
--- www/js/input/checkLineCollision.js
+++ www/js/input/checkLineCollision.js
@@ -1,6 +1,32 @@
 define(function () {
-    function checkLineCollision() {
-        return false;
+    /**
+     * checks if a pointer hits a line
+     *
+     * @param {Object} line given line
+     * @param {Object} pointer given pointer
+     * @returns {boolean} true if it's a hit else false
+     */
+    function checkLineCollision(line, pointer) {
+        var lineVector = {
+            x: line.xPointB - line.xPointA,
+            y: line.yPointB - line.yPointA
+        };
+        var magnitude = Math.sqrt(lineVector.x * lineVector.x + lineVector.y * lineVector.y);
+
+        var normalLineUnitVector = {
+            x: -(lineVector.y /magnitude),
+            y: lineVector.x / magnitude
+        };
+
+        var pointAToCircleVector = {
+            x: pointer.xPoint - line.xPointA,
+            y: pointer.yPoint - line.yPointA
+        };
+
+        var perpendicularLength = normalLineUnitVector.x * pointAToCircleVector.x +
+            normalLineUnitVector.y * pointAToCircleVector.y;
+
+        return perpendicularLength <= pointer.radius;
     }
 
     return checkLineCollision;
