203c82f14a63f969afa5851eb4afb89132f4309b
raphaelstary <raphael.stary@gmail.com>
2014-01-21 12:53:32 +0100

    line action interpreter case point A & point B

--- test/spec/input/LineActionInterpreterSpec.js
+++ test/spec/input/LineActionInterpreterSpec.js
@@ -8,10 +8,11 @@ define(['input/LineActionInterpreter', 'input/PointerAction'], function (LineAct
                 yPoint: 500,
                 radius: 2
             };
-            var circle = {
-                xPoint: 100,
-                yPoint: 100,
-                radius: 50
+            var line = {
+                xPointA: 100,
+                yPointA: 100,
+                xPointB: 150,
+                yPointB: 100
             };
             var checkCollision = function () {
                 return false;
@@ -19,37 +20,61 @@ define(['input/LineActionInterpreter', 'input/PointerAction'], function (LineAct
 
             var cut = new LineActionInterpreter(checkCollision);
 
-            var actual = cut.interpret(pointer, circle);
+            var actual = cut.interpret(pointer, line);
 
             expect(actual).toBe(PointerAction.NOTHING);
         });
 
-//        it("should return 'MOVE' " +
-//            "when I hit the center point", function () {
-//            var pointer = {
-//                xPoint: 100,
-//                yPoint: 100,
-//                radius: 2
-//            };
-//            var circle = {
-//                xPoint: 100,
-//                yPoint: 100,
-//                radius: 50
-//            };
-//            var checkCollision = function (actionPoint, pointer) {
-//                return actionPoint.xPoint === circle.xPoint && actionPoint.yPoint === circle.yPoint;
-//            };
-//
-//            var cut = new CircleActionInterpreter(checkCollision);
-//
-//            var actual = cut.interpret(pointer, circle);
-//
-//            expect(actual).toBe(PointerAction.MOVE);
-//        });
+        it("should return 'CHANGE_POINT_A' " +
+            "when I hit point A", function () {
+            var pointer = {
+                xPoint: 100,
+                yPoint: 100,
+                radius: 2
+            };
+            var line = {
+                xPointA: 100,
+                yPointA: 100,
+                xPointB: 150,
+                yPointB: 100
+            };
+            var checkCollision = function (actionPoint, pointer) {
+                return actionPoint.xPoint === line.xPointA && actionPoint.yPoint === line.yPointA;
+            };
+
+            var cut = new LineActionInterpreter(checkCollision);
+
+            var actual = cut.interpret(pointer, line);
+
+            expect(actual).toBe(PointerAction.CHANGE_POINT_A);
+        });
     });
 
     describe("as a user I click on the 'point B' action point of a line to start the edit action", function () {
 
+        it("should return 'CHANGE_POINT_B' " +
+            "when I hit point B", function () {
+            var pointer = {
+                xPoint: 150,
+                yPoint: 100,
+                radius: 2
+            };
+            var line = {
+                xPointA: 100,
+                yPointA: 100,
+                xPointB: 150,
+                yPointB: 100
+            };
+            var checkCollision = function (actionPoint, pointer) {
+                return actionPoint.xPoint === line.xPointB && actionPoint.yPoint === line.yPointB;
+            };
+
+            var cut = new LineActionInterpreter(checkCollision);
+
+            var actual = cut.interpret(pointer, line);
+
+            expect(actual).toBe(PointerAction.CHANGE_POINT_B);
+        });
     });
 
     describe("as a user I click on 'control point A' action point of a line to start the " +
--- www/js/input/LineActionInterpreter.js
+++ www/js/input/LineActionInterpreter.js
@@ -1,9 +1,17 @@
 define(['input/PointerAction'], function (PointerAction) {
+    var OFF_SET = 2;
+
     function LineActionInterpreter(checkCollision) {
         this._checkCollision = checkCollision;
     }
 
     LineActionInterpreter.prototype.interpret = function (pointer, line) {
+        if (this._checkCollision({xPoint: line.xPointA, yPoint: line.yPointA, radius: OFF_SET}, pointer))
+            return PointerAction.CHANGE_POINT_A;
+
+        else if (this._checkCollision({xPoint: line.xPointB, yPoint: line.yPointB, radius: OFF_SET}, pointer))
+            return PointerAction.CHANGE_POINT_B;
+
         return PointerAction.NOTHING;
     };
 
