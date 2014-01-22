03d4bb32c33dbe3195d1975bfff1aabb91e5082f
raphaelstary <raphael.stary@gmail.com>
2014-01-22 11:32:22 +0100

    line action interpreter

--- test/spec/input/LineActionInterpreterSpec.js
+++ test/spec/input/LineActionInterpreterSpec.js
@@ -129,37 +129,37 @@ define(['input/LineActionInterpreter', 'input/PointerAction'], function (LineAct
         });
     });
 
-//    describe("as a user I click on 'control point B' action point of a line to start the " +
-//        "edit action: transform to bezier curve", function() {
-//
-//        it("should return 'TRANSFORM_FROM_POINT_B " +
-//            "when I hit control point A", function () {
-//            var pointer = {
-//                center: {
-//                    xPoint: 160,
-//                    yPoint: 100
-//                },
-//                radius: 2
-//            };
-//            var line = {
-//                pointA: {
-//                    xPoint: 100,
-//                    yPoint: 100
-//                },
-//                pointB: {
-//                    xPoint: 150,
-//                    yPoint: 100
-//                }
-//            };
-//            var checkCollision = function (actionPoint, pointer) {
-//                return actionPoint.center.xPoint === 160 && actionPoint.center.yPoint === 100;
-//            };
-//
-//            var cut = new LineActionInterpreter(checkCollision);
-//
-//            var actual = cut.interpret(pointer, line);
-//
-//            expect(actual).toBe(PointerAction.TRANSFORM_FROM_POINT_B);
-//        });
-//    });
+    describe("as a user I click on 'control point B' action point of a line to start the " +
+        "edit action: transform to bezier curve", function() {
+
+        it("should return 'TRANSFORM_FROM_POINT_B " +
+            "when I hit control point A", function () {
+            var pointer = {
+                center: {
+                    xPoint: 160,
+                    yPoint: 100
+                },
+                radius: 2
+            };
+            var line = {
+                pointA: {
+                    xPoint: 100,
+                    yPoint: 100
+                },
+                pointB: {
+                    xPoint: 190,
+                    yPoint: 100
+                }
+            };
+            var checkCollision = function (actionPoint, pointer) {
+                return actionPoint.center.xPoint === 160 && actionPoint.center.yPoint === 100;
+            };
+
+            var cut = new LineActionInterpreter(checkCollision);
+
+            var actual = cut.interpret(pointer, line);
+
+            expect(actual).toBe(PointerAction.TRANSFORM_FROM_POINT_B);
+        });
+    });
 });
--- www/js/input/LineActionInterpreter.js
+++ www/js/input/LineActionInterpreter.js
@@ -1,4 +1,4 @@
-define(['input/PointerAction'], function (PointerAction) {
+define(['input/PointerAction', 'math/Vectors'], function (PointerAction, Vectors) {
     var OFF_SET = 2;
 
     function LineActionInterpreter(checkCollision) {
@@ -12,9 +12,35 @@ define(['input/PointerAction'], function (PointerAction) {
         else if (this._checkCollision({center: {xPoint: line.pointB.xPoint, yPoint: line.pointB.yPoint}, radius: OFF_SET}, pointer))
             return PointerAction.CHANGE_POINT_B;
 
-//        else if ()
+        else {
+            var vector = Vectors.createVector(line.pointA, line.pointB);
+            var length = Vectors.magnitude(vector) / 3;
+            var unitVector = Vectors.normalize(vector);
 
-        return PointerAction.NOTHING;
+            var controlPointA = {
+                center: {
+                    xPoint: line.pointA.xPoint + length * unitVector.x,
+                    yPoint: line.pointA.yPoint + length * unitVector.y
+                },
+                radius: OFF_SET
+            };
+
+            var controlPointB = {
+                center: {
+                    xPoint: line.pointB.xPoint + length * -unitVector.x,
+                    yPoint: line.pointB.yPoint + length * -unitVector.y
+                },
+                radius: OFF_SET
+            };
+
+            if (this._checkCollision(controlPointA, pointer))
+                return PointerAction.TRANSFORM_FROM_POINT_A;
+
+            else if (this._checkCollision(controlPointB, pointer))
+                return PointerAction.TRANSFORM_FROM_POINT_B;
+
+        }
+            return PointerAction.NOTHING;
     };
 
     return LineActionInterpreter;
