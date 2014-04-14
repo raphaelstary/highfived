e78f7f04952057e75e26e2fb5137a910eda10c06
raphaelstary <raphael.stary@gmail.com>
2014-04-14 16:18:05 +0200

    factory 1st refactoring from layer to entity model

--- www/js/model/Factory.js
+++ www/js/model/Factory.js
@@ -1,7 +1,5 @@
-define(['lib/knockout', 'model/Layer', 'model/Rectangle', 'model/LayerBucket'], function (ko, Layer, Rectangle,
-    LayerBucket) {
-    function Factory(list) {
-        this.list = list;
+define(['lib/knockout', 'model/LayerBucket'], function (ko, LayerBucket) {
+    function Factory() {
     }
 
     Factory.prototype.createLayerModel = function () {
@@ -11,24 +9,6 @@ define(['lib/knockout', 'model/Layer', 'model/Rectangle', 'model/LayerBucket'], 
 
         var result = ko.observableArray();
         result.forEach = forEach;
-
-        if (this.list === undefined || this.list === null || this.list.length === 0) {
-            return new LayerBucket(result);
-        }
-
-        this.list.forEach(function (layer) {
-            var items = ko.observableArray();
-            items.forEach = forEach;
-
-            if (layer.items !== undefined && layer.items !== null && layer.items.length > 0) {
-
-                layer.items.forEach(function(item) {
-                    items.push(new Rectangle(item.name, item.xPoint, item.yPoint, item.width, item.height));
-                });
-            }
-
-            result.push(new Layer(layer.name, items, layer.type));
-        });
 
         return new LayerBucket(result);
     };
