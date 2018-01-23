4c3eefb4566fb299e1dbab3a5e6ca95632864e4e
raphael <raphael.stary@gmail.com>
2018-01-23 12:22:20 -0800

    adding project name & scenes list to html mockup

--- src/main.js
+++ src/main.js
@@ -1,64 +1,43 @@
 import initView from './view/initView.js';
 
-const startScene = {
-    'screen': {
-        'width': 750,
-        'height': 1334
-    },
-    'drawables': [
-        {
-            'type': 'rectangle',
-            'x': 375,
-            'y': 667,
-            'width': 750,
-            'height': 1334,
-            'referenceId': 29,
-            'filled': false,
-            'lineWidth': 1,
-            'color': '#000000'
-        }, {
-            'type': 'text',
-            'x': 373,
-            'y': 974,
-            'width': 121,
-            'height': 78,
-            'referenceId': 6,
-            'alpha': 0.4,
-            'msg': 'play',
-            'rotation': 0,
-            'font': 'FFF Forward',
-            'fontStyle': 'Regular',
-            'size': 48,
-            'color': '#f4f4f4'
-        }, {
-            'type': 'text',
-            'x': 373,
-            'y': 1172,
-            'width': 367,
-            'height': 39,
-            'referenceId': 26,
-            'alpha': 0.2,
-            'msg': 'a game by raphael stary',
-            'rotation': 0,
-            'font': 'FFF Forward',
-            'fontStyle': 'Regular',
-            'size': 24,
-            'color': '#f4f4f4'
-        }, {
-            'type': 'text',
-            'x': 373,
-            'y': 388,
-            'width': 579,
-            'height': 75,
-            'referenceId': 5,
-            'alpha': 0.8,
-            'msg': 'PONGY PADDLE',
-            'rotation': 0,
-            'font': 'FFF Forward',
-            'fontStyle': 'Regular',
-            'size': 60,
-            'color': '#f4f4f4'
+window.onload = () => {
+    let startScene;
+
+    const canvas = document.getElementById('screen');
+    const app = H5.Bootstrapper.build((services) => {
+        const scenes = new H5.SceneMap();
+
+        const testSceneKey = 'my-test';
+        const scene = new H5.MVVMScene(services, services.startScene, {}, testSceneKey);
+
+        scenes.put(testSceneKey, scene.show.bind(scene));
+        scenes.next(testSceneKey);
+
+        services.events.fire(H5.Event.RESIZE, {
+            width: services.startScene.screen.width,
+            height: services.startScene.screen.height,
+            cssWidth: services.startScene.screen.width,
+            cssHeight: services.startScene.screen.height,
+            devicePixelRatio: 1
+        });
+
+        services.screen.style.width = services.startScene.screen.width + 'px';
+        services.screen.style.height = services.startScene.screen.height + 'px';
+
+        services.screen.style.backgroundColor = services.startScene.screen.color;
+
+        initView(services.startScene);
+
+    }, {
+        load: (resourceLoader) => {
+            startScene = resourceLoader.addJSON('../asset/scene/test.json');
+
+        },
+        process: (services) => {
+            services.startScene = startScene;
         }
-    ]
+    }, canvas);
+
+    app.start();
+
 };
-window.onload = () => initView(startScene);
